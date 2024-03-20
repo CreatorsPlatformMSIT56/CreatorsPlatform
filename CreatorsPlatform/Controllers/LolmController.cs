5e63d7e3e9e621306c0c0090cdd0bc7658833daa
royam9 <lolm19070@gmail.com>
2024-03-20 17:34:45 +0800

    推送錯誤解決

--- CreatorsPlatform/Controllers/LolmController.cs
+++ CreatorsPlatform/Controllers/LolmController.cs
@@ -4,62 +4,62 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CreatorsPlatform.Controllers
 {
-    public class LolmController : Controller
-    {
-        private readonly ImaginkContext _context;
+	public class LolmController : Controller
+	{
+		private readonly ImaginkContext _context;
 
-        public LolmController(ImaginkContext context)
-        {
-            _context = context;
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public LolmController(ImaginkContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
 
 
-        //public IActionResult EventContent()
-        //{
-        //    return View();
-        //}
+		//public IActionResult EventContent()
+		//{
+		//    return View();
+		//}
 
-        // 新增存 id 的變數，並想把他丟到 View
-        public async Task<IActionResult> EventContent(int? id)
-        {
-            TempData["Eid"] = id;
-            var imaginkContext = _context.EventsAndImages;
+		// 新增存 id 的變數，並想把他丟到 View
+		public async Task<IActionResult> EventContent(int? id)
+		{
+			TempData["Eid"] = id;
+			var imaginkContext = _context.EventsAndImages;
 
-            // 原本想在 controller 寫 LINQ，但不會傳到 view
-            //IEnumerable<EventsAndImage> query = from CCC in imaginkContext
-            //                                    where CCC.EventId == id
-            //                                    select CCC;
+			// 原本想在 controller 寫 LINQ，但不會傳到 view
+			//IEnumerable<EventsAndImage> query = from CCC in imaginkContext
+			//                                    where CCC.EventId == id
+			//                                    select CCC;
 
-            
 
-            return View(await imaginkContext.ToListAsync());
-        }
 
-        public IActionResult CreateEvent()
-        {
-            return View();
-        }
+			return View(await imaginkContext.ToListAsync());
+		}
 
-        // 
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		public IActionResult CreateEvent()
+		{
+			return View();
+		}
 
-            var eventsAndImages = await _context.EventsAndImages
-                .FirstOrDefaultAsync(m => m.EventId == 1);
-            if (eventsAndImages == null)
-            {
-                return NotFound();
-            }
+		// 
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            return View(eventsAndImages);
-        }
-    }
+			var eventsAndImages = await _context.EventsAndImages
+				.FirstOrDefaultAsync(m => m.EventId == 1);
+			if (eventsAndImages == null)
+			{
+				return NotFound();
+			}
+
+			return View(eventsAndImages);
+		}
+	}
 }
