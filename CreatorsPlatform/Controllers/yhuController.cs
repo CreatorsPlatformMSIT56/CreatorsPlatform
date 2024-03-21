1b00e7d84e3e5d38e313a77d1efdc5e793df70ca
Scott50266 <sasdqsdq@gmail.com>
2024-03-21 11:46:39 +0800

    #

--- CreatorsPlatform/Controllers/yhuController.cs
+++ CreatorsPlatform/Controllers/yhuController.cs
@@ -2,12 +2,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.VisualBasic;
 using Newtonsoft.Json;
+using NuGet.Protocol;
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using static CreatorsPlatform.Controllers.HomeController;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using CreatorsPlatform.CsMod.yhu;
 
 namespace CreatorsPlatform.Controllers
 {
@@ -19,7 +23,12 @@ namespace CreatorsPlatform.Controllers
         {
             _context = context;
         }
+		private readonly LinqFunctionReserve _LinqGet;
 
+        public yhuController(LinqFunctionReserve LinqGet)
+        {
+            _LinqGet = LinqGet;
+        }
 
 
 
@@ -54,54 +63,119 @@ namespace CreatorsPlatform.Controllers
 					new Dftest { Nickname = "David Taylor2tay", ImageURL = "https://example.com/image8.jpg", Title = "Ut enim ad minim veniam", Description = "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
 					new Dftest { Nickname = "Jennifer Anderson2tay", ImageURL = "https://example.com/image9.jpg", Title = "Excepteur sint occaecat cupidatat non proident", Description = "Sunt in culpa qui officia deserunt mollit anim id est laborum." }
 				};
-		//LINQ方法(TEST)
-		public List<Dftest> newsListLinq(int x, int y, List<Dftest> ListName)
-		{
-			if (ListName.Count < y)
-			{
-				y = ListName.Count;
-			}
-			List<Dftest> Form1 = new List<Dftest>();
-			for (int i = x; i < y; i++)
-			{
-				Form1.Add(ListName[i]);
-			}
-			return Form1;
-		}
 		public IActionResult PersonalUser()
 		{
             var memberJson = HttpContext.Session.GetString("key");
             MemberData member = JsonConvert.DeserializeObject<MemberData>(memberJson);
-            return View(member);
+			var UserPasswd=(from UserPasswdData in _context.Users 
+							where UserPasswdData.Email==member.Email
+							select new { UserPasswdData .Password}).ToList();
+            if (member.Password == UserPasswd[0].Password.ToString())
+            {
+				var NewmesgData =(from Newmesg in _context.Contents
+                                  select new { Newmesg.TagId, Newmesg.Description, Newmesg.ImageUrl,Newmesg.UploadDate}).OrderByDescending(item => item.UploadDate).Take(5);
+                return View("PersonalUser", NewmesgData);
+            }
+			else
+			{
+                return View("Login");
+            };
+			
 		}
 		[HttpPost]
-		public ActionResult PersonalUser(int _CurrentMsg, string tapy)
-		{
-			List<Dftest> updateList = new List<Dftest>() { };
-			switch (tapy)
-			{
-				case "newmsg":
-					updateList = newsListLinq(_CurrentMsg, _CurrentMsg + 10, newmsg);
-					break;
-				case "subscribemsg":
-					updateList = newsListLinq(_CurrentMsg, _CurrentMsg + 10, subscribemsg);
-					break;
-			}
-			return Json(updateList);
-		}
+		//public ActionResult PersonalUser(int _CurrentMsg, string tapy)
+		//{
+		//	switch (tapy)
+		//	{
+		//		case "newmsg":
+		//			var NewmesgData = (from Newmesg in _context.Contents
+		//							   select new { Newmesg.TagId, Newmesg.Description, Newmesg.ImageUrl, Newmesg.UploadDate })
+		//							   .OrderByDescending(item => item.UploadDate).Skip(_CurrentMsg).Take(5);
+		//			var updateList = NewmesgData.ToList();
+
+  //                  break;
+		//		case "subscribemsg":
+  //                  var memberJson = HttpContext.Session.GetString("key");
+  //                  MemberData member = JsonConvert.DeserializeObject<MemberData>(memberJson);
+               
+
+  //                  var SubscriptionDataList = (from SubscriptionData in _context.Subscriptions
+		//										where SubscriptionData.UserId== _LinqGet.GetUserid(member)
+
+
+
+
+  //                                                  );
+
+  //                  break;
+		//	}
+		//	//return Json(updateList);
+		//}
 		public IActionResult IMAGINK()
         {
-            var ContentsData = 
-								from CreatorsData in  _context.Creators
-                                join UserData in _context.Users on CreatorsData.CreatorId equals UserData.CreatorId
-                                select new { UserData.UserName, UserData.Avatar, CreatorsData.CreatorId, CreatorsData.Description };
+        //    var ContentsData = 
+								//from CreatorsData in  _context.Creators
+        //                        join UserData in _context.Users on CreatorsData.CreatorId equals UserData.CreatorId
+        //                        select new { UserData.UserName, UserData.Avatar, CreatorsData.CreatorId, CreatorsData.Description };
+			var DefaultCreatorsData =
+							 (from DefaultContents in _context.Contents
+                              group DefaultContents by DefaultContents.CreatorId into PopularityRranking
+                              select new
+                              {
+                                  UserID = PopularityRranking.Key,
+                                  UserLikes = PopularityRranking.Sum(r => r.Likes)
+                              }).OrderByDescending(item => item.UserLikes).Take(6);
+            var DefaultCreatorsDataList = DefaultCreatorsData.ToList();
+			Console.WriteLine(DefaultCreatorsData);
 
-
+            var DefaultContentsData = (from DefaultContents in _context.Contents
+									   where DefaultContents.CreatorId == DefaultCreatorsDataList[0].UserID
+									   select new { DefaultContents.ImageUrl, DefaultContents.Title,
+										   DefaultContents.Description,DefaultContents.UploadDate 
+									   }).OrderByDescending(item => item.UploadDate).Take(3);
+			var DefaultContentsDataList = DefaultContentsData.ToList();
+            Console.WriteLine(DefaultContentsData);
 
 
             return View();
 		}
-		public IActionResult Payment()
+        [HttpPost]
+		public ActionResult IMAGINK(int x)
+		{
+            var DefaultCreatorsData =
+                             (from DefaultContents in _context.Contents
+							  where DefaultContents.CategoryId == x
+                              group DefaultContents by DefaultContents.CreatorId into PopularityRranking
+                              select new
+                              {
+                                  UserID = PopularityRranking.Key,
+                                  UserLikes = PopularityRranking.Sum(r => r.Likes)
+                              }).OrderByDescending(item => item.UserLikes).Take(6);
+            var DefaultCreatorsDataList = DefaultCreatorsData.ToList();
+            Console.WriteLine(DefaultCreatorsData);
+			return Json(DefaultCreatorsDataList);
+        }
+        [HttpPost]
+        public ActionResult IMAGINK(string x)
+        {
+			var CreatorsID = (from Creators in _context.Users
+							  where Creators.UserName == x
+							  select  Creators );
+			var CreatorsList = CreatorsID.ToList();
+             var DefaultContentsData = (from DefaultContents in _context.Contents
+                                       where DefaultContents.CreatorId == CreatorsList[0].CreatorId
+                                        select new
+                                       {
+                                           DefaultContents.ImageUrl,
+                                           DefaultContents.Title,
+                                           DefaultContents.Description,
+                                           DefaultContents.UploadDate
+                                       }).OrderByDescending(item => item.UploadDate).Take(3);
+            var DefaultContentsDataList = DefaultContentsData.ToList();
+            Console.WriteLine(DefaultContentsData);
+            return Json(DefaultContentsData);
+        }
+        public IActionResult Payment()
 		{
 			return View();
 		}
