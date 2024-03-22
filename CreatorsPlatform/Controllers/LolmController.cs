b3562549fd38d4fafe345b9f64db899a067b58bf
royam9 <lolm19070@gmail.com>
2024-03-22 16:37:37 +0800

    CreateEvent上資料庫初步成功

--- CreatorsPlatform/Controllers/LolmController.cs
+++ CreatorsPlatform/Controllers/LolmController.cs
@@ -1,6 +1,9 @@
 ﻿using CreatorsPlatform.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace CreatorsPlatform.Controllers
 {
@@ -16,7 +19,6 @@ namespace CreatorsPlatform.Controllers
 		{
 			return View();
 		}
-
 
 		//public IActionResult EventContent()
 		//{
@@ -35,67 +37,129 @@ namespace CreatorsPlatform.Controllers
 			//                                    select CCC;
 
 			// 取得TempData裡面裝的字串
-			ViewBag.QuillContent = TempData.Peek("DataFromClient");			
+			ViewBag.QuillContent = TempData.Peek("DataFromClient");
 			return View(await imaginkContext.ToListAsync());
 		}
 
 		public IActionResult CreateEvent()
 		{
+
 			return View();
 		}
 
-		[HttpPost]
-		public string CreateEvent(string DataFromClient)
-		{
-			return DataFromClient;
-		}
 
 		// 取得input的值並上傳到資料庫
+		//[HttpPost]
+		//public async Task<IActionResult> Upload([FromBody]Event model)
+		//{
+		//	if (ModelState.IsValid)
+		//	{
+		//		// 從 ViewModel 中獲取表單數據
+		//		//var eventName = model.EventName;
+		//		//var startDate = model.StartDate;
+		//		//var endDate = model.EndDate;
+
+		//		// 先不要處理圖片
+		//		//var banner = model.Banner;
+
+		//		// 先不要處理描述
+		//		// var description = model.Description;
+
+		//		//var eventStyle = model.EventStyle;
+
+		//		// 創建 Event 對象並賦值
+		//		//var NewEvent = new Event
+		//		//{
+		//		//	EventName = eventName,
+		//		//	StartDate = startDate,
+		//		//	EndDate = endDate,
+		//			// Banner = banner,
+		//			// Description = description,
+		//		//	EventStyle = eventStyle
+		//		//};
+
+		//		try
+		//		{
+		//			// 將對象添加到資料庫並保存更改
+		//			_context.Events.Add(model);
+		//			await _context.SaveChangesAsync();
+		//			return Ok();
+		//			//return RedirectToAction(nameof(EventContent));
+		//		}
+		//		catch (Exception ex)
+		//		{
+		//			// 處理例外情況
+		//			ModelState.AddModelError("", $"無法保存圖片：{ex.Message}");
+		//		}
+		//	}
+		//	// 如果模型狀態無效，返回表單頁面
+		//	return View(model);
+		//}
+
+		// 品旭的
 		[HttpPost]
-		public async Task<IActionResult> Upload(Event model)
+		public async Task<IActionResult> Upload([Bind("EventImageId,ImageUrl,EventId,ImageSample,CreatorId,Description,ImageTitle")] EventImage eventImage, int? id)
 		{
-			if (ModelState.IsValid)
+			foreach (var file in HttpContext.Request.Form.Files)
 			{
-				// 從 ViewModel 中獲取表單數據
-				var eventName = model.EventName;
-				var startDate = model.StartDate;
-				var endDate = model.EndDate;
-
-				// 先不要處理圖片
-				//var banner = model.Banner;
-
-				// 先不要處理描述
-				// var description = model.Description;
-				
-				var eventStyle = model.EventStyle;
-
-				// 創建 Event 對象並賦值
-				var NewEvent = new Event
+				if (file.Length > 0)
 				{
-					EventName = eventName,
-					StartDate = startDate,
-					EndDate = endDate,
-					// Banner = banner,
-					// Description = description,
-					EventStyle = eventStyle
-				};
+					//var name = HttpContext.Request.Form["name"];
+					//var description = HttpContext.Request.Form["description"];
 
-				try
-				{
-					// 將對象添加到資料庫並保存更改
-					_context.Events.Add(NewEvent);
-					await _context.SaveChangesAsync();
-					return RedirectToAction(nameof(EventContent));
-				}
-				catch (Exception ex)
-				{
-					// 處理例外情況
-					ModelState.AddModelError("", $"無法保存圖片：{ex.Message}");
+					using (var ms = new MemoryStream())
+					{
+						await file.CopyToAsync(ms);
+						var imageData = Convert.ToBase64String(ms.ToArray());
+						var dataURL = $"data:{file.ContentType};base64,{imageData}";
+						//var dataEvt = (int)id!;
+						//int nameInInt = Convert.ToInt32(name);
+						//int descriptionInInt = Convert.ToInt32(description);
+
+						// 將DataURL保存到資料庫
+						var photo = new EventImage
+						{
+							ImageUrl = dataURL,
+							CreatorId = 7,
+							EventId = 3
+						};
+						try
+						{
+							_context.EventImages.Add(photo);
+							await _context.SaveChangesAsync();
+							return RedirectToAction(nameof(EventContent));
+						}
+						catch (Exception)
+						{
+							_context.EventImages.Add(photo);
+							await _context.SaveChangesAsync();
+						}
+					}
 				}
 			}
+			return RedirectToAction(nameof(EventContent));
+		}
 
-			// 如果模型狀態無效，返回表單頁面
-			return View(model);
+		[HttpPost]
+		[Route("Lolm/Create")]
+		public IActionResult Create([FromBody] JsonElement json)
+		{
+			Event @event = new Event
+			{
+				EventName = json.GetProperty("EventName").GetString(),
+				StartDate = json.GetProperty("StartDate").GetDateTime(),
+				EndDate = json.GetProperty("EndDate").GetDateTime(),
+				Description = json.GetProperty("Description").GetString(),
+				CategoryId = json.GetProperty("CategoryID").GetInt32()
+			};
+			if (ModelState.IsValid != null)
+			{
+				_context.Add(@event);
+				_context.SaveChanges();
+				return Ok(); // 返回成功狀態碼 200
+			}
+
+			return BadRequest(); // 返回錯誤狀態碼 400
 		}
 
 		public async Task<IActionResult> Details(int? id)
--- CreatorsPlatform/wwwroot/js/Lolm/CreateEvent.js
+++ CreatorsPlatform/wwwroot/js/Lolm/CreateEvent.js
@@ -57,5 +57,74 @@ function getQuillContent() {
 
 // 拿到所有的input內容並新增到資料庫
 function PostAllToSQL() {
+    //var eventName = $("#eventName").val();
+    //var startDate = $("#startDate").val();
+    //var endDate = $("#endDate").val();
+    //var eventStyle = $("#eventStyle").val();
+    //var data = {
+    //    EventName: eventName,
+    //    StartDate: startDate,
+    //    EndDate: endDate,
+    //    EventStyle: eventStyle
+    //};
 
+    //var dataFromClient = {
+    //    EventName: '阿巴巴',
+    //    StartDate: "2024-03-22T10:04:35.123",
+    //    EndDate: "2024-03-23T10:04:35.123",
+    //    Description: "低死哭順",
+    //    CategoryID: 1
+    //};
+
+    var dataFromClient = {
+        EventName: $("#eventName").val(),
+        StartDate: $("#startDate").val(),
+        EndDate: $("#endDate").val(),
+        Description: "低死哭順",
+        CategoryID: 1
+    };
+
+    $.ajax({
+        url: "/Lolm/Create",
+        method: "post",
+        contentType: 'application/json',
+        data: JSON.stringify(dataFromClient),
+        success: function (response) {
+            console.log(response);
+            alert(response);
+        },
+        error: function (xhr, status, error) {
+            // 處理錯誤 
+            alert("fail");
+        }
+     });
+}
+
+function PostAllToSQL02() {
+    var startDate = new Date("2024-03-22T10:04:35.123").toISOString();
+    var endDate = new Date("2024-03-22T10:05:35.123").toISOString();
+
+    $.ajax({
+        url: '/Lolm/Create',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            EventName: '阿巴巴',
+            StartDate: startDate,
+            EndDate: endDate,
+            Description: "低死哭順",
+            CategoryID: 1
+        }),
+        success: function (response) {
+            // 處理成功回應
+            alert('成功');
+        },
+        error: function (xhr, status, error) {
+            // 處理錯誤回應
+            alert('失敗');
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+        }
+    });
 }
