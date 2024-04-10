61a4541d3e8cfa064d094da55154b35962a8a759
WarotaWorks <warotasandesu@gmail.com>
2024-04-11 00:01:19 +0800

    首頁bug修正

--- CreatorsPlatform/wwwroot/js/yhu/IMAGINK.js
+++ CreatorsPlatform/wwwroot/js/yhu/IMAGINK.js
@@ -46,7 +46,9 @@ function CreatorsChange(Category) {
 		}
 	});
 };
-
+function WokLink(x) {
+	window.location.href = `/Creator/GetPost/${x}`;
+};
 function WorkChanges(e) {
 	let match = e.id.match(/\d+/);
 	let intValue = parseInt(match[0], 10);
@@ -63,20 +65,14 @@ function WorkChanges(e) {
 			//---------------------------
 			$("#Woklist3img").empty();
 			//---------------------------
-
-
-
-
-
 			$("#Woklist1img").append(
-				`<a class="dropdown-item"${response[0].contentId != undefined ? `href="/Creator/GetPost/${response[0].contentId}` : ` href=""`}>			
-				<div class="ToMiddle image-container">
+				
+				`<div onclick = "WokLink(${response[0].contentId != undefined ? `${response[0].contentId}` : `1`})" class="ToMiddle image-container">
 							 ${response[0].imageUrl != undefined ? `<img src="data:image/png;base64,${response[0].imageUrl}" class="d-block WorkImg" alt="eoijwpeifjsj"> ` :
 							`<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-brush d-block WorkImg" viewBox="0 0 16 16">
 							 <path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.1 6.1 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.1 8.1 0 0 1-3.078.132 4 4 0 0 1-.562-.135 1.4 1.4 0 0 1-.466-.247.7.7 0 0 1-.204-.288.62.62 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896q.19.012.348.048c.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04M4.705 11.912a1.2 1.2 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.4 3.4 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3 3 0 0 0 .126-.75zm1.44.026c.12-.04.277-.1.458-.183a5.1 5.1 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005zm3.582-3.043.002.001h-.002z" />
 						     </svg>` }
 				   </div>
-				 </a>
 				 <div id="Woklist1text" class="carousel-caption d-none d-md-block TextDiv">
 							<h3 class="text-start fw-bold">${response[0].title != undefined ? response[0].title : ` `}</h5>
 							 <h5 class="text-end">${response[0].uploadDate != undefined ? response[0].uploadDate : ` `}</p>
@@ -84,14 +80,12 @@ function WorkChanges(e) {
 				 `	);
 			//----------------------------
 			$("#Woklist2img").append(
-				`<a class="dropdown-item"   ${response[1].contentId != undefined ? `href="/Creator/GetPost/${response[1].contentId}` : ` href=""`}>
-					<div class="ToMiddle image-container">
+				`<div onclick = "WokLink(${response[1].contentId != undefined ? `${response[1].contentId}` : `1`})" class="ToMiddle image-container">
 							${response[1].imageUrl != undefined ? `<img src="data:image/png;base64,${response[1].imageUrl}" class="d-block WorkImg" alt="eoijwpeifjsj"> ` :
 							`<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-brush d-block WorkImg" viewBox="0 0 16 16" class="d-block WorkImg">
 							<path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.1 6.1 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.1 8.1 0 0 1-3.078.132 4 4 0 0 1-.562-.135 1.4 1.4 0 0 1-.466-.247.7.7 0 0 1-.204-.288.62.62 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896q.19.012.348.048c.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04M4.705 11.912a1.2 1.2 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.4 3.4 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3 3 0 0 0 .126-.75zm1.44.026c.12-.04.277-.1.458-.183a5.1 5.1 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005zm3.582-3.043.002.001h-.002z" />
 							</svg>` }
 					</div>
-				</a>
 				<div id="Woklist2text" class="carousel-caption d-none d-md-block TextDiv">
 					<h3 class="text-start fw-bold">${response[1].title != undefined ? response[1].title : ` `}</h5>
 					 <h5 class="text-end">${response[1].uploadDate != undefined ? response[1].uploadDate : ` `}</p>
@@ -99,14 +93,12 @@ function WorkChanges(e) {
 			);
 			//-----------------------------
 			$("#Woklist3img").append(
-				`<a class="dropdown-item" ${response[2].contentId != undefined ? `href="/Creator/GetPost/${response[2].contentId}` : `href="" `}>
-					<div class="ToMiddle image-container">
+				`<div onclick = "WokLink(${response[2].contentId != undefined ? `${response[2].contentId}` : `1`})" class="ToMiddle image-container">
 						${response[2].imageUrl != undefined ? `<img src="data:image/png;base64,${response[2].imageUrl}" class="d-block WorkImg" alt="eoijwpeifjsj"> ` :
 						`<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-brush d-block WorkImg" viewBox="0 0 16 16" class="d-block WorkImg">
 								<path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.1 6.1 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.1 8.1 0 0 1-3.078.132 4 4 0 0 1-.562-.135 1.4 1.4 0 0 1-.466-.247.7.7 0 0 1-.204-.288.62.62 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896q.19.012.348.048c.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04M4.705 11.912a1.2 1.2 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.4 3.4 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3 3 0 0 0 .126-.75zm1.44.026c.12-.04.277-.1.458-.183a5.1 5.1 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005zm3.582-3.043.002.001h-.002z" />
 						</svg>` }
 					</div>
-				</a>
 				<div id="Woklist3text" class="carousel-caption d-none d-md-block TextDiv">
 					<h3 class="text-start fw-bold">${response[2].title != undefined ? response[2].title : ` `}</h5>
 				    <h5 class="text-end">${response[2].uploadDate != undefined ? response[2].uploadDate : ` `}</p>
